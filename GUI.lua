5d775b421bcb048cc5e98372401ad55186e5d48a
Focstor <111702496+Focstor@users.noreply.github.com>
2022-08-23 00:23:22 +0300

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -257,7 +257,9 @@ atm()
 end
 end)
 
-
+Section:NewButton("Infinity Jump", "tp", function()
+    -- //~ F to toggle ~\ -- -- I have no idea who made this!  _G.infinjump = true   local Player = game:GetService("Players").LocalPlayer local Mouse = Player:GetMouse() Mouse.KeyDown:connect(function(k) if _G.infinjump then if k:byte() == 32 then Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid") Humanoid:ChangeState("Jumping") wait(0.1) Humanoid:ChangeState("Seated") end end end)   local Player = game:GetService("Players").LocalPlayer local Mouse = Player:GetMouse() Mouse.KeyDown:connect(function(k) k = k:lower() if k == "f" then if _G.infinjump == true then _G.infinjump = false else _G.infinjump = true end end end)
+end)
 
 local Tab = Window:NewTab("Teleports")
 
