cf3a4362349433e7b185b6879b051b82595fe2ce
Focstor <111702496+Focstor@users.noreply.github.com>
2022-08-23 00:58:06 +0300

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -14,8 +14,26 @@ Section:NewSlider("Jumppower", "Jump", 500, 0, function(g) -- 500 (Макс. з�
 end)
 
 
-Section:NewButton("Infinity Jump (F toogle)", "tp", function()
-    -- //~ F to toggle ~\ -- -- I have no idea who made this!  _G.infinjump = true   local Player = game:GetService("Players").LocalPlayer local Mouse = Player:GetMouse() Mouse.KeyDown:connect(function(k) if _G.infinjump then if k:byte() == 32 then Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid") Humanoid:ChangeState("Jumping") wait(0.1) Humanoid:ChangeState("Seated") end end end)   local Player = game:GetService("Players").LocalPlayer local Mouse = Player:GetMouse() Mouse.KeyDown:connect(function(k) k = k:lower() if k == "f" then if _G.infinjump == true then _G.infinjump = false else _G.infinjump = true end end end)
+Section:NewButton("Infinity Jump ", "tp", function()
+   
+	local Player = game:GetService'Players'.LocalPlayer;
+	local UIS = game:GetService'UserInputService';
+
+	_G.JumpHeight = 50;
+ 
+	function Action(Object, Function) if Object ~= nil then Function(Object); end end
+	UIS.InputBegan:connect(function(UserInput)
+		if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then
+			Action(Player.Character.Humanoid, function(self)
+				if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then
+					Action(self.Parent.HumanoidRootPart, function(self)
+						self.Velocity = Vector3.new(0, _G.JumpHeight, 0);
+					end)
+				end
+			end)
+		end
+	end)
+end)
 end)
 
 local Tab = Window:NewTab("Misc")
